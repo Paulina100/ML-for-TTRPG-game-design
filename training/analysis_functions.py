d4c7947fb31cff71896fbb623e3a40015194a4b1
tunczyk101 <jolka2102@gmail.com>
2023-04-14 23:44:34 +0200

    refactor analysis_function.py - to training

--- training/analysis_functions.py
+++ training/analysis_functions.py
@@ -1,12 +1,14 @@
-from typing import Union
+from typing import Union, List, TypeVar
 
 import pandas as pd
 import numpy as np
 import pandas.core.frame
 import pandas.core.series
 
+DataFrameType = TypeVar("pandas.core.frame.DataFrame")
 
-def import_merged_bestiaries(books: list[str]) -> pandas.core.frame.DataFrame:
+
+def import_merged_bestiaries(books: List[str]) -> DataFrameType:
     """
     Returns merged books
 
@@ -23,8 +25,8 @@ def import_merged_bestiaries(books: list[str]) -> pandas.core.frame.DataFrame:
 
 
 def unpack_column(
-    df: pandas.core.frame.DataFrame, column_name: str, print_info: bool = False
-) -> Union[pandas.core.frame.DataFrame, None]:
+    df: DataFrameType, column_name: str, print_info: bool = False
+) -> Union[DataFrameType, None]:
     # sth wrong with Series
     """
     Unpacks dict and changes it to DataFrame
@@ -44,24 +46,3 @@ def unpack_column(
         print(new_df.iloc[0])
         print(f"Len = {len(new_df.iloc[0])}")
     return new_df
-
-
-def remove_redundant_level(
-    df: pandas.core.frame.DataFrame, columns: list[str], value: str
-) -> pandas.core.frame.DataFrame:
-    # TODO
-    # find why it doesn't work
-    """
-    Modifies dataframe and returns it.
-    For every row of df and column from columns changes value of cell to value of dictionary that is in that cell to
-     remove redundant level in df.
-
-    :param df: dataframe
-    :param columns: list of columns to modify
-    :param value: value we are looking for in dict in cell
-    :return: df with changed cells
-    """
-    for col in columns:
-        for i, row in df.iterrows():
-            row[col] = row[col].get(value)
-    return df
