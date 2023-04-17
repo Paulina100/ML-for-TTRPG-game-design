8388e8bc6d2f353d2a40475c63ecebd5d975e2c3
tunczyk101 <jolka2102@gmail.com>
2023-04-17 12:55:34 +0200

    adds linaer_regression and random_forest

--- training/create_model.py
+++ training/create_model.py
@@ -2,11 +2,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LassoCV
 import pandas.core.frame
 import pandas.core.series
-
+from sklearn.model_selection import GridSearchCV
 
 CLASSIFIERS = {
     "train_linear_regression": LassoCV(n_alphas=1000, random_state=0),
-    "train_random_forest": RandomForestClassifier(random_state=0, n_jobs=-1),
+    "train_random_forest": RandomForestClassifier(
+        random_state=0, n_jobs=-1
+    ),  # czy jakiś inny? RandomForestRegressor?
 }
 
 
@@ -22,7 +24,7 @@ def create_model(
     :param classifier_name: name of a chosen classifier
     :return: trained classifier of a chosen type
     """
-    clf = CLASSIFIERS.get(classifier_name)
+    clf = create_clf_with_tuning(classifier_name)
 
     if clf is None:
         return
@@ -34,16 +36,28 @@ def create_model(
     return clf
 
 
-def _tuning(clf, classifier_name: str):
+def create_clf_with_tuning(classifier_name: str):
     """
     tuning the hyper-parameters of an estimator
     :param clf: classifier
     :param classifier_name: name of classifier
     :return: classifier after tuning
     """
-    if classifier_name == "train_linear_regression":
-        return clf
+    base_clf = CLASSIFIERS.get(classifier_name)
+    if classifier_name == "train_linear_regression" or base_clf is None:
+        return base_clf
 
-    # other cases
-    # gridsearch??
+    hyper_params = {
+        "n_estimators": [100, 200, 300, 400, 500],
+        "max_features": ["sqrt", "log2", 0.1, 0.2, 0.3, 0.4, 0.5, None],
+        "max_depth": [3, 6, 9],
+        "max_leaf_nodes": [3, 6, 9],
+    }
+    clf = GridSearchCV(
+        estimator=base_clf,
+        param_grid=hyper_params,
+        verbose=2,
+        return_train_score=True,
+    )
+
     return clf
