16048fb354e22a79e18a6bfffab98ed54d9d6398
Paulina100 <paulina.jedrychowska@gmail.com>
2023-05-19 13:23:34 +0200

    commit before rebase on dataset/create_model

--- training/save_load_model_to_file.py
+++ training/save_load_model_to_file.py
@@ -4,6 +4,12 @@ from training.creating_dataset import create_dataframe
 
 
 def save_model_to_file(model=None, filename: str = "../saved_models/current_model.pkl"):
+    """
+    Saves a machine learning model to a file
+    :param model: machine learning model to be saved. If None, default model will be created and trained
+    :param filename: path and filename to save the model to
+    """
+
     if model is None:
         X = create_dataframe()
         X.pop("book")
@@ -19,6 +25,11 @@ def save_model_to_file(model=None, filename: str = "../saved_models/current_mode
 
 
 def load_model_from_file(filename: str = "../saved_models/current_model.pkl"):
+    """
+    Loads a machine learning model from a file
+    :param filename: path and filename of the model file to load
+    :return: loaded machine learning model
+    """
     return joblib.load(filename)
 
 
