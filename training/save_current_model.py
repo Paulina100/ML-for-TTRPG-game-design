4474d684b3f5ba2227084c49802cad338b6f3d01
Paulina100 <paulina.jedrychowska@gmail.com>
2023-06-13 22:43:15 +0200

    Added script for saving current model

---
+++ training/save_current_model.py
@@ -0,0 +1,12 @@
+import joblib
+
+from training.create_model import get_fitted_model
+from training.creating_dataset import create_dataframe
+
+
+X = create_dataframe()
+X.pop("book")
+y = X.pop("level")
+rf = get_fitted_model(classifier_name="random_forest", X_train=X, y_train=y)
+
+joblib.dump(value=rf, filename="../saved_models/current_model.pkl")
