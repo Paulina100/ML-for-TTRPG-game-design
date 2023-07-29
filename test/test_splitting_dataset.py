18a9e38950bc8894133c701945d47d2a6f669e40
Paulina100 <paulina.jedrychowska@gmail.com>
2023-07-29 15:04:44 +0200

    fixed tests

--- test/test_splitting_dataset.py
+++ test/test_splitting_dataset.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 
 from training.creating_dataset import load_and_preprocess_data
@@ -10,6 +12,15 @@ from training.splitting_dataset import (
 )
 
 
+DATASETS_DIR = pathlib.Path(__file__).parent.parent / "pathfinder_2e_data"
+DATASET_FILES = [
+    "pathfinder-bestiary.db",
+    "pathfinder-bestiary-2.db",
+    "pathfinder-bestiary-3.db",
+]
+DATASET_PATHS = [f"{DATASETS_DIR}/{file}" for file in DATASET_FILES]
+
+
 # get_grouped_book_names
 def test_get_date_books_mapping():
     grouped_books = get_date_books_mapping()
@@ -19,7 +30,21 @@ def test_get_date_books_mapping():
 # get_dataframe_with_oldest_books
 def test_get_dataframe_with_oldest_books():
     test_size = 0.5
-    df = load_and_preprocess_data()
+    df = load_and_preprocess_data(
+        DATASET_PATHS,
+        characteristics=[
+            "cha",
+            "con",
+            "dex",
+            "int",
+            "str",
+            "wis",
+            "ac",
+            "hp",
+            "focus",
+            "fortitude",
+        ],
+    )
     oldest_books_df = get_dataframe_with_oldest_books(df, test_size)
     assert oldest_books_df.shape[0] >= int((1 - test_size) * df.shape[0])
 
@@ -33,7 +58,21 @@ def test_get_dataframe_with_oldest_books():
 # get_chronological_split_results
 def test_get_chronological_split_results():
     test_size = 0.5
-    df = load_and_preprocess_data()
+    df = load_and_preprocess_data(
+        DATASET_PATHS,
+        characteristics=[
+            "cha",
+            "con",
+            "dex",
+            "int",
+            "str",
+            "wis",
+            "ac",
+            "hp",
+            "focus",
+            "fortitude",
+        ],
+    )
     X_train, X_test, y_train, y_test = get_chronological_split_results(df, test_size)
     assert "level" not in X_train
     assert "level" not in X_test
@@ -50,7 +89,21 @@ def test_get_chronological_split_results():
 # _get_random_split_results
 def test_get_random_split_results():
     test_size = 0.5
-    df = load_and_preprocess_data()
+    df = load_and_preprocess_data(
+        DATASET_PATHS,
+        characteristics=[
+            "cha",
+            "con",
+            "dex",
+            "int",
+            "str",
+            "wis",
+            "ac",
+            "hp",
+            "focus",
+            "fortitude",
+        ],
+    )
     X_train, X_test, y_train, y_test = get_random_split_results(df, test_size)
     assert "level" not in X_train
     assert "level" not in X_test
@@ -61,7 +114,21 @@ def test_get_random_split_results():
 # split_dataframe
 def test_split_dataframe():
     test_size = 1.5
-    df = load_and_preprocess_data()
+    df = load_and_preprocess_data(
+        DATASET_PATHS,
+        characteristics=[
+            "cha",
+            "con",
+            "dex",
+            "int",
+            "str",
+            "wis",
+            "ac",
+            "hp",
+            "focus",
+            "fortitude",
+        ],
+    )
     with pytest.raises(ValueError):
         X_train, X_test, y_train, y_test = split_dataframe(df, test_size)
 
