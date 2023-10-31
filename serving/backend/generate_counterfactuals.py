3909ce3706d8770a349337fef9860a89e3ac75c4
tunczyk101 <jolka2102@gmail.com>
2023-10-31 17:13:59 +0100

    ignore warning, remove 'modified' from generate_counterfactuals

--- serving/backend/generate_counterfactuals.py
+++ serving/backend/generate_counterfactuals.py
@@ -1,12 +1,19 @@
 import json
 import os
 import sys
+import warnings
+
+
+warnings.filterwarnings("ignore")
+# Ignore warning: UserConfigValidationException will be deprecated from dice_ml.utils.
+# Please import UserConfigValidationException from raiutils.exceptions.
 
 import dice_ml
 import pandas as pd
 from dice_ml import Dice
-from raiutils.exceptions import UserConfigValidationException
 
+
+warnings.resetwarnings()
 
 sys.path.insert(0, os.sep.join(os.path.normpath(__file__).split(os.sep)[:-1]))
 from constants import ORDERED_CHARACTERISTICS, THRESHOLD
@@ -19,16 +26,17 @@ def generate_counterfactuals(
     df: pd.DataFrame,
     total_cf: int = 5,
     ordered_characteristics: list[str] = ORDERED_CHARACTERISTICS,
-) -> dict[str, float]:
+) -> dict[str, list]:
     """
     Generate counterfactual explanations for a monster's characteristics to achieve a new level.
 
+    :param ordered_characteristics: The order of characteristics in the given model.
     :param monster_stats: A dictionary containing the monster's current characteristics and values.
     :param model: The machine learning model used for prediction.
     :param new_level: The target level the monster should achieve.
     :param df: A pandas DataFrame containing other monsters' data for generating explanations.
     :param total_cf: The number of counterfactual explanations to generate.
-    :return: A dictionary containing the generated counterfactual explanations *values* and information whether characteristic was *modified*.
+    :return: A dictionary containing the generated counterfactual explanations *values*.
     """
     query = pd.DataFrame.from_records([monster_stats])
     query = query[ordered_characteristics]
@@ -51,18 +59,16 @@ def generate_counterfactuals(
 
     result = {
         "values": [],
-        "modified": [],
     }
-    # query can consist of more than one instance
+    # Query can consist of more than one instance
     # so each data about original stats and counterfactuals is given as a list of lists - we always have one instance
-    # "test_data": [[[1.0, 5.0, 2.0, 1.0, 7.0, 2.0, 29.0, 215.0, 10.231964445152919]]]
-    original = cf_json["test_data"][0][0]  # list(monster_stats.values())
+    # "cfs_list": [[[1.0, 5.0, 2.0, 1.0, 7.0, 2.0, 29.0, 215.0, 10.231964445152919]]]
 
     for cf in cf_json["cfs_list"][0]:
         if cf[-1] != new_level - 1 + THRESHOLD:
+            # If the predicted level is equal to `new_level - 1 + THRESHOLD`,
+            # `calculate_level` would qualify this counterfactual for new_level - 1, not new_level.
             cf = cf[:-1]
             result["values"].append(cf)
-            cf_modified = [val != original[i] for i, val in enumerate(cf)]
-            result["modified"].append(cf_modified)
 
     return result
