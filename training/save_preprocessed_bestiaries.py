34d4f9758c7f27cd62481e965de06aa954e43a31
tunczyk101 <jolka2102@gmail.com>
2023-12-11 18:59:59 +0100

    update characteristics order, model for full

--- training/save_preprocessed_bestiaries.py
+++ training/save_preprocessed_bestiaries.py
@@ -1,7 +1,8 @@
 import os
 import pathlib
 
-from serving.backend.constants import ORDERED_CHARACTERISTICS_50
+from serving.backend.constants import ORDERED_CHARACTERISTICS_FULL
+from training.constants import FEATURES
 from training.creating_dataset import load_and_preprocess_data
 
 
@@ -14,53 +15,12 @@ DATASET_FILES = [
 ]
 
 DATASET_PATHS = [f"{DATASETS_DIR}/{file}" for file in DATASET_FILES]
-FEATURES = [
-    "cha",
-    "con",
-    "dex",
-    "int",
-    "str",
-    "wis",
-    "ac",
-    "hp",
-    "perception",
-    "fortitude",
-    "reflex",
-    "will",
-    "focus",
-    "land_speed",
-    "num_immunities",
-    "fly",
-    "swim",
-    "climb",
-    "fire_resistance",
-    "cold_resistance",
-    "electricity_resistance",
-    "acid_resistance",
-    "piercing_resistance",
-    "slashing_resistance",
-    "physical_resistance",
-    "bludgeoning_resistance",
-    "mental_resistance",
-    "poison_resistance",
-    "all-damage_resistance",
-    "cold-iron_weakness",
-    "good_weakness",
-    "fire_weakness",
-    "cold_weakness",
-    "area-damage_weakness",
-    "splash-damage_weakness",
-    "evil_weakness",
-    "slashing_weakness",
-    "melee",
-    "ranged",
-    "spells",
-]
+
 if __name__ == "__main__":
     df = load_and_preprocess_data(
         DATASET_PATHS,
         characteristics=FEATURES,
     )
 
-    df = df[ORDERED_CHARACTERISTICS_50]
+    df = df[ORDERED_CHARACTERISTICS_FULL]
     df.to_csv("../counterfactual_datasets/bestiaries_full.csv")
