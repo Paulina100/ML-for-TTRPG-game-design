2027b277e8bfe4627e05008b5e0cb8d56ad4b1c4
bogumilap <bogumila.papiernik@gmail.com>
2023-12-17 21:59:08 +0100

    add info about level not calculated successfully

--- serving/frontend/src/Components/Results/Results.jsx
+++ serving/frontend/src/Components/Results/Results.jsx
@@ -8,11 +8,16 @@ const Results = ({results, monsterProperties, setMonsterProperties}) => {
             {renderHeader("Results")}
             {JSON.stringify(results) === "{}" ?
                 <p>Results will be calculated after you submit monster's properties.</p> :
-                <div>
-                    <Level level={results.level} />
-                    <CounterfactualExamples monsterProperties={monsterProperties}
-                                            setMonsterProperties={setMonsterProperties} />
-                </div>
+                results.level !== undefined ?
+                    <div>
+                        <Level level={results.level} />
+                        <CounterfactualExamples monsterProperties={monsterProperties}
+                                                setMonsterProperties={setMonsterProperties} />
+                    </div> :
+                    <p>Level was not calculated successfully. Please review your input and try again. If this error
+                        persists, you are welcome to <a href={"https://github.com/Paulina100/ML-for-TTRPG-game-design/issues/new"}
+                                                        target={"_blank"} rel={"noreferrer"}>open an issue on app's github</a>
+                        {" "}to let us know.</p>
             }
         </div>
     );
